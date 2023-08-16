4e1472d477f0dc3a3f4cbcd1ffc9cee69de5b871
anhng <anananh1999@gmail.com>
2023-08-16 19:42:07 +1000

    Create todo app

---
+++ src/components/TodoForm.js
@@ -0,0 +1,55 @@
+import React, {useState, useEffect, useRef} from 'react';
+function TodoForm(props) {
+  const inputFocus = useRef(null);
+  useEffect(() => {
+    inputFocus.current.focus();
+  });
+    const [input, setInput] = useState(props.edit ? props.edit.value : '');
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        props.onSubmit({
+            id: Math.floor(Math.random() * 10000),
+            text: input,
+            isCompleted: false
+        })
+        setInput('');
+    };
+    const handleChange = (e) => {
+        setInput(e.target.value);
+    };
+    return (
+      <form onSubmit={handleSubmit} className='todo-form'>
+      {props.edit ? (
+        <>
+          <input
+            placeholder='Update your item'
+            value={input}
+            onChange={handleChange}
+            name='text'
+            ref={inputFocus}
+            className='todo-input edit'
+          />
+          <button onClick={handleSubmit} className='todo-button edit'>
+            Update
+          </button>
+        </>
+      ) : (
+        <>
+          <input
+            placeholder='Add a todo'
+            value={input}
+            onChange={handleChange}
+            name='text'
+            className='todo-input'
+            ref={inputFocus}
+          />
+          <button onClick={handleSubmit} className='todo-button'>
+            Add todo
+          </button>
+        </>
+    )}
+    </form>
+    )
+  }
+  
+  export default TodoForm;
